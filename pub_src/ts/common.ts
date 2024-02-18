98d72467807d3fdd34d713522bf4fef95437dcce
Lasy <lasyard@yeah.net>
2024-02-18 15:40:15 +0800

    Add more kanjis in zhCmp.

--- pub_src/ts/common.ts
+++ pub_src/ts/common.ts
@@ -19,7 +19,8 @@ export function numCmp(a: string, b: string) {
  */
 export function zhToNum(str: string) {
     const map: { [key: string]: number } = {
-        '零': 0, '一': 1, '二': 2, '三': 3, '四': 4, '五': 5, '六': 6, '七': 7, '八': 8, '九': 9, '十': 10
+        '零': 0, '一': 1, '二': 2, '三': 3, '四': 4, '五': 5, '六': 6, '七': 7, '八': 8, '九': 9, '十': 10,
+        '〇': 0, '壹': 1, '贰': 2, '叁': 3, '肆': 4, '伍': 5, '陆': 6, '柒': 7, '捌': 8, '玖': 9, '拾': 10,
     };
     let result: number | null = null;
     for (let i = 0; i < str.length; ++i) {
