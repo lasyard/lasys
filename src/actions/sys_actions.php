d6de22bef1d013c6f9e636679a880a0f523870ae
Lasy <lasyard@yeah.net>
2025-12-24 17:34:33 +0800

    add SysActions::xdebugInfo

--- src/actions/sys_actions.php
+++ src/actions/sys_actions.php
@@ -5,4 +5,14 @@ final class SysActions extends Actions
     {
         phpinfo();
     }
+
+    public function actionXdebugInfo()
+    {
+        if (function_exists('xdebug_info')) {
+            // call dynamically to avoid undefined function errors in static analysis
+            call_user_func('xdebug_info');
+        } else {
+            echo 'Xdebug is not installed.';
+        }
+    }
 }
